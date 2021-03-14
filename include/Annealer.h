49bdcbadba75ca6cfd6354b5f3b5f08b840b7ae8
Samuel Gonzalez Checa <samuel.gonzalezcheca@outlook.com>
2021-03-13 23:59:13 -0600

    Calculate fitness score

--- include/Annealer.h
+++ include/Annealer.h
@@ -8,11 +8,13 @@ class Annealer{
     int numberOfStudents = 200;
     int numberOfRooms = 50;
     int studentsPerRoom = 4;
+    std::array<int,50> fitnessScore;
     std::array<int, 200> rooms;
     std::array<int, 40000> studentsCompatibility;
     public:
     Annealer(std:: array<int, 40000> studentsCompatibility);
     void AssignRooms();
     void outputResult();
+    void CalculateFitnessScore(int roomCounter);
 };
 #endif
--- src/Annealer.cpp
+++ src/Annealer.cpp
@@ -5,24 +5,38 @@ Annealer::Annealer(std::array<int,40000> studentsCompatibility){
     AssignRooms();
 }
 void Annealer::AssignRooms(){
-    for (int i = 0; i < numberOfRooms*studentsPerRoom; i++)
-    {
+    for (int i = 0; i < numberOfRooms*studentsPerRoom; i++){
         rooms[i] = i;
     }
+    for (int currentRooom = 0; currentRooom < numberOfRooms; currentRooom++)
+    {
+        CalculateFitnessScore(currentRooom);
+    }
     
+
 }
 void Annealer::outputResult(){
     std::ofstream outputFile;
     outputFile.open("data/output.txt");
-    for (int i = 0; i < numberOfRooms; i++)
-    {
+    for (int i = 0; i < numberOfRooms; i++){
         outputFile << "Room " << i << ": " << rooms[i*studentsPerRoom] 
         << " " << rooms[i*studentsPerRoom +1] 
         << " " << rooms[i*studentsPerRoom +2]
         << " " << rooms[i*studentsPerRoom +3]
-
-        
-        << std::endl;
+        << " Fitness Score: " << fitnessScore[i] << "\n";
     }
     
+}
+
+void Annealer::CalculateFitnessScore(int roomCounter){
+    int score = 0;
+    for (int i = 0; i < studentsPerRoom; i++){
+            for (int j = i+1; j < studentsPerRoom; j++)
+            {
+                int row = roomCounter*studentsPerRoom+i;
+                int col = roomCounter*studentsPerRoom + j;
+                score += studentsCompatibility[rooms[row]*200 + rooms[col]];
+            }
+    }
+    fitnessScore[roomCounter] = score;
 }
