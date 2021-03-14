0ce318d5ba63e4dd70a9a8dc8c2cb7db9f526302
Samuel Gonzalez Checa <samuel.gonzalezcheca@outlook.com>
2021-03-13 18:45:24 -0600

    Assign rooms

--- include/Annealer.h
+++ include/Annealer.h
@@ -6,10 +6,12 @@ class Annealer{
     private:
     int numberOfStudents = 200;
     int numberOfRooms = 50;
-    int studentsPerRooms = 4;
+    int studentsPerRoom = 4;
+    std::array<int, 200> rooms;
     std::array<int, 40000> studentsCompatibility;
     public:
     Annealer(std:: array<int, 40000> studentsCompatibility);
+    void AssignRooms();
 
 };
 #endif
--- src/Annealer.cpp
+++ src/Annealer.cpp
@@ -1,1 +1,12 @@
 #include "Annealer.h"
+
+Annealer::Annealer(std::array<int,40000> studentsCompatibility){
+    this->studentsCompatibility = studentsCompatibility;
+}
+void Annealer::AssignRooms(){
+    for (int i = 0; i < numberOfRooms*studentsPerRoom; i++)
+    {
+        rooms[i] = i;
+    }
+    
+}
