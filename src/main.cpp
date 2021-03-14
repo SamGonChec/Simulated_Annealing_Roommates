8733cb918fa3ad1a128afeef6a5e5df823058896
Samuel Gonzalez Checa <samuel.gonzalezcheca@outlook.com>
2021-03-13 18:13:54 -0600

    Read file successfully

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <fstream>
+#include <array>
 
 int main(){
+    std::ifstream roommatesInput;
+    roommatesInput.open("docs/roommates.txt");
+    std::array<int, 40000> studentsCompatibilityArray;
+    if (!roommatesInput)
+    {
+        return -1;
+    }
+    
+    for (int i = 0; i < 40000; i++)
+    {
+        roommatesInput >> studentsCompatibilityArray[i];
+        std::cout << studentsCompatibilityArray[i] << " ";
+    }
+    
+
     std::cout << "HOLA MUNDO\n";
     return 0;
 }
